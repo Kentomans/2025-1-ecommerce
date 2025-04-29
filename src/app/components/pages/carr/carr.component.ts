a008fa294e4a5fd19eb7df0bcbb1fb1cde5a90a5
Angel Sanchez <angelsancezs@email.com>
2025-04-28 21:42:10 -0600

    part1

---
+++ src/app/components/pages/carr/carr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carr',
+  imports: [],
+  templateUrl: './carr.component.html',
+  styleUrl: './carr.component.css'
+})
+export class CarrComponent {
+
+}
