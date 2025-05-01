99de2f1b913d010a6360e7a31544f79c4be8d203
Angel Sanchez <angelsancezs@email.com>
2025-04-30 18:03:06 -0600

    rutas p1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/pages/product-list/product-list.component';
 import { CarrComponent } from './components/pages/carr/carr.component';
 import { CheckoutComponent } from './components/pages/checkout/checkout.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 export const routes: Routes = [
+    { path: "home", component:ContentComponent, title:"GameStore"},
     { path: 'productos', component:ProductListComponent , title:"Productos" },
     { path: 'carrito', component:CarrComponent , title:"Carrito" },
     { path: 'checkout', component:CheckoutComponent, title:"CheckOut"},
+    { path: '', component:ContentComponent, title:"GameStore"},
+    { path: '**', redirectTo:'/home', title:"GameStore"},
 ];
--- src/app/components/pages/checkout/checkout.component.ts
+++ src/app/components/pages/checkout/checkout.component.ts
@@ -31,26 +31,27 @@ export class CheckoutComponent {
 
       onSubmit():void{
         if(this.adress.valid){
+          Swal.fire({
+            title: "La informacion es correcta?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
         }
       }
 
 
 
-      // Swal.fire({
-      //   title: "La informacion es correcta?",
-      //   text: "You won't be able to revert this!",
-      //   icon: "warning",
-      //   showCancelButton: true,
-      //   confirmButtonColor: "#3085d6",
-      //   cancelButtonColor: "#d33",
-      //   confirmButtonText: "Yes, delete it!"
-      // }).then((result) => {
-      //   if (result.isConfirmed) {
-      //     Swal.fire({
-      //       title: "Deleted!",
-      //       text: "Your file has been deleted.",
-      //       icon: "success"
-      //     });
-      //   }
-      // });
+   
 }
